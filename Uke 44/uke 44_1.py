3b0f22120830f640ef43cfc54df587858ccd7193
Torstein Ekroll <torsteinem@gmail.com>
2024-10-31 14:41:43 +0100

    Bugfix

--- Uke 44/uke 44_1.py
+++ Uke 44/uke 44_1.py
@@ -1,4 +1,4 @@
-(a, b) = [int() for x in input("Skriv inn en brøk A/B: ").split("/")]
+(a, b) = [int(x) for x in input("Skriv inn en brøk A/B: ").split("/")]
 while b > 0:
     (a, b) = (b, a % b)
 print(f"Brøken kan forkortes med {a}.") 
