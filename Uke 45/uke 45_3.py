026972ee6b7dbf2e99b34f41ee029013109ca1d2
Torstein Ekroll <torsteinem@gmail.com>
2024-11-08 23:25:03 +0100

    Endring på html-streng

--- Uke 45/uke 45_3.py
+++ Uke 45/uke 45_3.py
@@ -1,5 +1,5 @@
 from flask import Flask
 webApp = Flask(__name__)
 @webApp.route("/")
-def landing(): return "<h2>Hello World</h2>"
+def landing(): return """<p>For å komme i gang med Flask, kan du følge <a href="https://flask.palletsprojects.com/en/latest/quickstart/#a-minimal-application" target="_blank">Minimal Application Guide</a> på Flask sine sider.</p>"""
 webApp.run(debug=True)
