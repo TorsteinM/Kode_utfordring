652d0f4ffb420e52c4e0b348b7366c8251b47441
Torstein Ekroll <torsteinem@gmail.com>
2024-11-12 08:25:38 +0100

    La til kommentar

--- Uke 46/uke 46_1.py
+++ Uke 46/uke 46_1.py
@@ -1,6 +1,6 @@
 (n, i, l) = (int(input("Skriv inn et heltall: ")), 2, [])
 while n > 1:
     while n % i == 0:
-        (n, _) = (n // i, l.append(i))
+        (n, _) = (n // i, l.append(i)) # _ brukes ofte for forkaste et resultat
     i += 1
 print(f"Primtallsfaktorisering: {", ".join([str(x) for x in l])}")
